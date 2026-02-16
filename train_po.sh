7d6d5d735dda96d39ce55013b086b0c89207e1e9
gsd_g53 <gsd2003_20@163.com>
2026-02-16 08:42:34 +0000

    update train log

--- train_po.sh
+++ train_po.sh
@@ -1,3 +1,3 @@
 nohup python -u run.py experiment=routing/pomo-po4cops-tsp100-po \
   ~callbacks.learning_rate_monitor ~callbacks.rich_progress_bar \
-  > train_po.log 2>&1 &
+  > train_po.out 2>&1 &
