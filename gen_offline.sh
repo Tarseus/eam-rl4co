3a30a3e5217dac67b8c2d44eb95ea74ecdbbedf2
gsd_g53 <gsd2003_20@163.com>
2026-02-28 15:34:29 +0000

    update log

---
+++ gen_offline.sh
@@ -0,0 +1,13 @@
+python scripts/eval_baseline_minitrain.py \
+  --config PTP/configs/experiment/pref_loss_coevo/alternating_simple.yaml \
+  --K 200 \
+  --train_problem_size 100 \
+  --valid_problem_sizes 100 \
+  --num_validation_episodes 512 \
+  --train_batch_size 64 \
+  --offline_train offline_data/tsp100_train.pt \
+  --offline_val 100 offline_data/tsp100_val.pt \
+  --scratch_init_seed 12345 \
+  --ckpt_135 baseline/epoch_135.ckpt \
+  --ckpt_409 baseline/epoch_409.ckpt \
+  --out baseline/mini_eval/baseline_minitrain_tsp100_K200.json
